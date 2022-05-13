2d71a9202397788f93ae4dc873f1512a4e51fae5
rikydwianto <rikydwianto04@gmail.com>
2022-05-13 09:22:01 +0700

    koneksi

--- php/config/koneksi.php
+++ php/config/koneksi.php
@@ -1,8 +1,6 @@
 <?php
-header("X-XSS-Protection: 1; mode=block");
-
-$con = mysqli_connect("localhost", $username, $password, $db_name);
+$con = mysqli_connect($host, $username, $password, $db_name);
 $kon = $con;
 // session_start();
-session_start();
+session_start(); 
 ?>
